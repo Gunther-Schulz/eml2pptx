9640d87ef13684a0b4b78c4dbce3b8cb87d7974a
Gunther Schulz <dev@guntherschulz.de>
2024-02-08 20:13:23 +0300

    add check wether the excel file is open before updating it

--- src/lib/xlsx_processing.py
+++ src/lib/xlsx_processing.py
@@ -6,14 +6,14 @@ import os
 import shutil
 
 config = load_config()
-xlsx_file = config['xlsx_file']
+xlsx_filepath = config['xlsx_file']
 xlsx_sheet_name = config['xlsx_sheet_name']
 xlsx_columns = config['xlsx_columns']
 
 
 # open xlsx file
 wb = Workbook()
-wb = load_workbook(filename=xlsx_file)
+wb = load_workbook(filename=xlsx_filepath)
 
 
 def get_cell_by_email(email):
@@ -54,13 +54,13 @@ def get_id(email):
 
 
 def save_xlsx_file():
-    xlsx_file_path = os.path.dirname(xlsx_file)
+    xlsx_file_path = os.path.dirname(xlsx_filepath)
     backup_dir = f'{xlsx_file_path}/backup'
     os.makedirs(backup_dir, exist_ok=True)
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    backup_file = f'{backup_dir}/{os.path.basename(xlsx_file)}_{timestamp}'
-    if shutil.copyfile(xlsx_file, backup_file):
-        wb.save(xlsx_file)
+    backup_file = f'{backup_dir}/{os.path.basename(xlsx_filepath)}_{timestamp}'
+    if shutil.copyfile(xlsx_filepath, backup_file):
+        wb.save(xlsx_filepath)
     else:
         print('Error: could not save backup file. Skipping saving xlsx file.')
 
@@ -89,5 +89,16 @@ def set_if_replied(email):
 def update_excel_file(emails):
     # update excel file with new data
 
+    # check that the excel file not not currently opened in the windows programm Excel. check by testing for the existence of the file starting with a tilde
+    filename = os.path.basename(xlsx_filepath)
+    directory = os.path.dirname(xlsx_filepath)
+
+    if os.path.exists(os.path.join(directory, f'~${filename}')):
+        print(
+            f'Error: Updating the Excel file failed. Excel file {xlsx_filepath} is currently open in Excel. Please close the file and try again.')
+        return
+
     for email in emails:
         set_if_replied(email)
+        # if not set_if_replied(email):
+        #     print(f'Warning: email {email} not found in xlsx file')
